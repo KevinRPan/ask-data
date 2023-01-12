a46c5c18b94dc8ead6e5e7eede6ae0326607e859
Kevin Pan <kevinrpan@gmail.com>
2023-01-11 23:08:10 -0500

    make output runnable

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import openai
 from datetime import datetime
 from streamlit.components.v1 import html
+import pandas as pd
+from pandasql import sqldf 
 
 st.set_page_config(page_title="Query Engine")
-
 
 html_temp = """
                 <div style="background-color:{};padding:1px">
@@ -12,18 +13,17 @@ html_temp = """
                 </div>
                 """
 
-
 with st.sidebar:
     st.markdown("""
     # About 
     Ask Data is a helper tool built using generative AI to help you answer questions about your data. 
-    
+
     Query Engine will enable every knowledge worker to unlock the powers of data science. 
     """)
     st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
     st.markdown("""
-    # Tips
-    Enter your table structure (table names, column names) and any questions you have about it.  
+    # Tips:
+    Enter your table structure (table names: column names) and any questions you have about it.  
     """)
     st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
     st.markdown("""
@@ -31,89 +31,103 @@ with st.sidebar:
     unsafe_allow_html=True,
     )
 
+    query_type = st.radio(
+        'Select an output preference',
+        options=['SQL', 'Python', 'Brainstorm!'])
+
 
 input_text_table, input_text_question = None, None
+
 if 'output' not in st.session_state:
     st.session_state['output'] = 0
 
-# if st.session_state['output'] <=2:
 st.markdown("""
 # Query Engine
 """)
-input_text_table = st.text_input("Table Structure", disabled=False)
 
-input_text_question = st.text_input("Your question", disabled=False)
+input_text_table = st.text_input("Enter your Table Structure", disabled=False, placeholder="Example format: fact_table: (date, id, val), dim_table: (id, feat, qual)")
+
+uploaded_file = st.file_uploader("Or choose a file")
+
+input_df_table = ''
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+    input_df_table = ', '.join([str(col) for col in df.columns])
+
+input_text_question = st.text_input("Your question", disabled=False, placeholder="Example: What is the weekly average val of feature?")
+
 st.session_state['output'] = st.session_state['output'] + 1
-# else:
-#     st.info("Try out your query!")
-#     st.markdown('''
-#     <style>
-#     .btn{
-#         display: inline-flex;
-#         -moz-box-align: center;
-#         align-items: center;
-#         -moz-box-pack: center;
-#         justify-content: center;
-#         font-weight: 400;
-#         padding: 0.25rem 0.75rem;
-#         border-radius: 0.25rem;
-#         margin: 0px;
-#         line-height: 1.6;
-#         color: #fff;
-#         background-color: #00acee;
-#         width: auto;
-#         user-select: none;
-#         border: 1px solid #00acee;
-#         }
-#     .btn:hover{
-#         color: #00acee;
-#         background-color: #fff;
-#     }
-#     </style>
-#     ''',
-#     unsafe_allow_html=True
-#     )
 
-# hide="""
-# <style>
-# footer{
-# 	visibility: hidden;
-#     position: relative;
-# }
-# .viewerBadge_container__1QSob{
-#     visibility: hidden;
-# }
-# <style>
-# """
-# st.markdown(hide, unsafe_allow_html=True)
+# st.write(st.session_state['output'])
 
-st.markdown(
-    """
-    <style>
-        iframe[width="220"] {
-            position: fixed;
-            bottom: 60px;
-            right: 40px;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-if input_text_table and input_text_question:
+table_structure = input_text_table + '\n' + input_df_table
+
+if (len(table_structure) > 5) and (len(input_text_question) > 5):
+
     prompt_prefix = "Using the table structure: "
-    prompt_suffix = "Write me a SQL query to to find: "
-    prompt = prompt_prefix + str(input_text_table) + prompt_suffix + str(input_text_question)
+
+    if query_type == 'SQL':
+        prompt_query = "Write me a SQL query to to find: "
+        prompt_suffix = '' 
+        output_type = 'SQL'
+        output_file_ext = '.sql'
+
+    elif query_type == 'Python': 
+        prompt_query = "You have a dataframe named df. Do not write an import or read file. Write me python code on the dataframe df using the pandas library to find: "
+        prompt_suffix = 'import pandas as pd \n'
+        output_type = 'Python'
+        output_file_ext = '.py'
+
+
+    elif query_type == 'Brainstorm!':
+        prompt_query = "Brainstorm a solution to find: "
+        prompt_suffix = ''
+        output_type = 'Text'
+        output_file_ext = '.md'
+
+
+    prompt = prompt_prefix \
+             + str(table_structure) \
+             + ' \n' \
+             + prompt_query \
+             + str(input_text_question) \
+             + ' \n' \
+             + prompt_suffix 
+
+    # st.write(prompt)
+
     if prompt:
         openai.api_key = st.secrets["openaiKey"]
-        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1000)
+        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1000, temperature=0.3, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
+
+        ## with tokens to spare, could shorten input text 
+
         question_output = response['choices'][0]['text']
-        today = datetime.today().strftime('%Y-%m-%d:%X')
+        today = datetime.today().strftime('%Y-%m-%d')
         topic = "Query help for: "+input_text_question+"\n@Date: "+str(today)+"\n"+question_output
-        
+
         st.info(question_output)
-        filename = "query_"+ str(input_text_question)+"_"+str(today)+".sql"
+
+        filename = "query_"+str(st.session_state['output'])+"_"+str(today)+ output_file_ext
         btn = st.download_button(
-            label="Download SQL",
+            label="Download " + output_type,
             data=topic,
             file_name=filename
         )
+    
+        if query_type == 'Python':
+            try:
+                eval_check = eval(question_output)
+                st.write(eval_check)
+            except Exception as e:
+                st.write(e.message)
+
+        elif query_type == 'SQL':
+            try:
+                pysqldf = lambda q: sqldf(q, globals())
+                sql_string = question_output.replace('table','df')
+                st.write(pysqldf(sql_string))
+            except Exception as e:
+                st.write(e)
