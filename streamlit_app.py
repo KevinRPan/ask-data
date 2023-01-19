45bbfd976c66c8ecd39850840ec9cd4990f0cf63
Kevin Pan <kevinrpan@gmail.com>
2023-01-18 20:48:05 -0500

    spinner while thinking, error if open ai is down

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,62 +150,72 @@ if (len(table_structure) > 5) and (len(input_text_question) > 5):
 
     if prompt:
         openai.api_key = st.secrets["openaiKey"]
-        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1000, temperature=0.3, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
 
-        ## with tokens to spare, could shorten input text 
-
-        question_output = response['choices'][0]['text']
-        today = datetime.today().strftime('%Y-%m-%d')
-        topic = "Query help for: "+input_text_question+"\n@Date: "+str(today)+"\n"+question_output
-
-        if query_type == "Brainstorm!":
-            question_output.replace('-','\n-')
-        st.info(question_output)
-
-        filename = "query_"+str(st.session_state['output'])+"_"+str(today)+ output_file_ext
-        btn = st.download_button(
-            label="Download " + output_type,
-            data=topic,
-            file_name=filename
-        )
-
-        # Evaluate the output 
-        df = upload_df if uploaded_file is not None else demo_df
-
-        explainer_prompt_prefix = "Please explain what the following code does: \n"
-        explainer_prompt = explainer_prompt_prefix + question_output
-        if query_type == 'Python':
+        with st.spinner('Thinking...'):
             try:
-                eval_check = eval(question_output)
-                st.write(eval_check)
-                explanation_response = openai.Completion.create(engine="text-davinci-002", prompt=explainer_prompt, max_tokens=1000, temperature=0.3, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
-                explanation_output = explanation_response['choices'][0]['text']
-                st.markdown("### Explanation")
-                st.write(explanation_output)
-            except Exception as e:
-                if is_debug_mode:
-                    st.write(e)
-                pass
+                response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1000, temperature=0.3, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
 
-        elif query_type == 'SQL':
-            try:
-                pysqldf = lambda q: sqldf(q, globals())
-                re_table_name = re.compile('FROM (\w*)', re.IGNORECASE)
 
-                sql_string = re_table_name.sub('FROM df', question_output)
+                question_output = response['choices'][0]['text']
+                today = datetime.today().strftime('%Y-%m-%d')
+                topic = "Query help for: "+input_text_question+"\n@Date: "+str(today)+"\n"+question_output
 
-                explanation_response = openai.Completion.create(engine="text-davinci-002", prompt=explainer_prompt, max_tokens=1000, temperature=0.3, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
-                explanation_output = explanation_response['choices'][0]['text']
-                
-                st.markdown("### Explanation")
-                st.write(explanation_output)
+                if query_type == "Brainstorm!":
+                    question_output.replace('-','\n-')
+                st.info(question_output)
 
-                if is_debug_mode:
-                    st.write(sql_string)
-                pass
-            
-                st.write(pysqldf(sql_string))
-            except Exception as e:
-                if is_debug_mode:
-                    st.write(e)
-                pass
+                filename = "query_"+str(st.session_state['output'])+"_"+str(today)+ output_file_ext
+                btn = st.download_button(
+                    label="Download " + output_type,
+                    data=topic,
+                    file_name=filename
+                )
+
+                # Evaluate the output 
+                df = upload_df if uploaded_file is not None else demo_df
+
+                explainer_prompt_prefix = "Please explain what the following code does: \n"
+                explainer_prompt = explainer_prompt_prefix + question_output + "\n"
+
+
+                if query_type == 'Python':
+                    try:
+                        eval_check = eval(question_output)
+                        st.write(eval_check)
+                        explanation_response = openai.Completion.create(engine="text-davinci-002", prompt=explainer_prompt, max_tokens=1000, temperature=0.3, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
+                        explanation_output = explanation_response['choices'][0]['text']
+                        st.markdown("### Explanation")
+                        st.write(explanation_output)
+                    except Exception as e:
+                        if is_debug_mode:
+                            st.write(e)
+                        pass
+
+                elif query_type == 'SQL':
+                    try:
+                        pysqldf = lambda q: sqldf(q, globals())
+                        re_table_name = re.compile('FROM (\w*)', re.IGNORECASE)
+
+                        sql_string = re_table_name.sub('FROM df', question_output)
+
+                        explanation_response = openai.Completion.create(engine="text-davinci-002", prompt=explainer_prompt, max_tokens=1000, temperature=0.3, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
+                        explanation_output = explanation_response['choices'][0]['text']
+                        
+                        st.markdown("### Explanation")
+                        st.write(explanation_output)
+
+                        if is_debug_mode:
+                            st.write(sql_string)
+                        pass
+                    
+                        st.write(pysqldf(sql_string))
+                    except Exception as e:
+                        if is_debug_mode:
+                            st.write(e)
+                        pass
+
+            except openai.error.RateLimitError:
+                st.error("Sorry, there are many requests at the moment! Please try again in a few minutes.")
+            finally:
+                pass 
+            ## with tokens to spare, could shorten input text 
